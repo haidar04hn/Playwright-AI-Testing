0be9a3c7d23a5b2197d4a7708b8ebe03f7e598f2
haidar04hn <haidar04hn@gmail.com>
2026-09-05 11:06:13 +0530

    Delete tests/SearchProduct.spec.js

--- tests/SearchProduct.spec.js
+++
@@ -1,5 +0,0 @@
-// test('positive: user can search for a product and view its details', async ({ page }) => {
-//   const demo = new DemoBlazePage(page);
-//   await demo.login('Haidarali', 'Test@123');
-//   await expect(demo.welcomeText).toHaveText('Welcome Haidarali');
-// });
