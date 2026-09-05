86791fd58fb483669ddba82ae7e439f9995368aa
haidar04hn <haidar04hn@gmail.com>
2026-09-05 11:21:32 +0530

    3 filess deleting

--- tests/cart.spec.js
+++
@@ -1,5 +0,0 @@
-// add a new test case
-// test('positive: user can add an item to the cart and verify it is added', async ({ page }) => {
-//   const demo = new DemoBlazePage(page);
-//   await demo.login('Haidarali', 'Test@123');
-//   await expect(demo.welcomeText).toHaveText('Welcome Haidarali');
